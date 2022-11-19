f9489af28d9850c52c4c2ab9c5857a05d11d717c
Ily1606 <no1.ily1606@gmail.com>
2022-11-20 02:04:10 +0700

    build: add modal edit, delete shortcut

--- src/components/Modal/ModalDeleteShortcut.vue
+++ src/components/Modal/ModalDeleteShortcut.vue
@@ -4,22 +4,14 @@
     @close-modal="$emit('closeModal')"
     :dialog-class="['max-w-md']"
   >
-    <template #header>{{ title }}</template>
+    <template #header>Bạn chắc chắn rằng sẽ xóa lối tắt này?</template>
     <template #body>
-      <form @submit.prevent="submit" id="formShortcut">
-        <div class="my-8 space-y-10">
-          <MakodaInput
-            custom-placeholder="Tên lối tắt"
-            v-model="formData.name"
-            :errors="v$.formData.name.$errors"
-          />
-          <MakodaInput
-            custom-placeholder="URL lối tắt"
-            v-model="formData.url"
-            :errors="v$.formData.url.$errors"
-          />
-        </div>
-      </form>
+      <div class="my-6">
+        <p class="text-red-500">
+          Chúng tôi sẽ xóa lối tắt này trên máy chủ đám mây.
+        </p>
+        <p>Bạn chắc chắn muốn xóa?</p>
+      </div>
     </template>
     <template #footer>
       <div class="mt-4 flex justify-between">
@@ -28,19 +20,20 @@
           class="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
           @click="$emit('closeModal')"
         >
-          Đóng
+          Hủy bỏ
         </button>
         <button
           class="inline-flex items-center gap-x-3 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:btn-blue-300 disabled:text-blue-500"
           form="formShortcut"
           :disabled="isLoading"
+          @click.prevent="submit"
         >
           <VueFontAwesome
             v-if="isLoading"
             icon="fa-solid fa-spinner-third"
             class="w-4 h-4 fill-blue-500 cursor-pointer animate-spin"
           />
-          Hoàn tất
+          Xác nhận
         </button>
       </div>
     </template>
@@ -48,16 +41,17 @@
 </template>
 <script>
 import UModal from "./UModal.vue";
-import { required, url } from "@vuelidate/validators";
-import MakodaInput from "../Input/MakodaInput.vue";
-import { useVuelidate } from "@vuelidate/core";
 export default {
   components: {
     UModal,
-    MakodaInput,
   },
   mixins: [UModal],
-  setup: () => ({ v$: useVuelidate() }),
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
   data() {
     return {
       isLoading: false,
@@ -65,22 +59,16 @@ export default {
         name: "",
         url: "",
       },
-      title: "Thêm lối tắt",
     };
   },
   methods: {
     async submit() {
-      const result = await this.v$.$validate();
-      if (!result) {
-        return;
-      }
       this.isLoading = true;
-      this.sendData();
-    },
-    async sendData() {
       this.$store
-        .dispatch("shortcut/addShortcut", {
-          payload: this.formData,
+        .dispatch("shortcut/deleteShortcut", {
+          params: {
+            id: this.formData.id,
+          },
         })
         .then(() => {
           this.$store.dispatch("shortcut/getShortcut");
@@ -91,13 +79,9 @@ export default {
         });
     },
   },
-  created() {},
-  validations() {
-    return {
-      formData: {
-        name: { required },
-        url: { required, url },
-      },
+  created() {
+    this.formData = {
+      ...this.data,
     };
   },
 };
